102a0f37587d2b7438eb169d584b7ff9f9622d9d
Alexander <alexander@iMac-Alexander.local>
2024-02-06 17:03:26 +0300

    forms

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import detail_post, list_posts
+from .views import PostListView, detail_post, list_posts
 
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', list_posts, name='post-list'),
+    path('', PostListView.as_view(), name='post-list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', detail_post, name='post-detail')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
 from .models import Post
 
@@ -7,7 +8,13 @@ def list_posts(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 2)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
     return render(request, 'blog/post/list.html', {'posts': posts})
 
@@ -15,3 +22,10 @@ def list_posts(request):
 def detail_post(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, publish__year=year, publish__month=month, publish__day=day, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
