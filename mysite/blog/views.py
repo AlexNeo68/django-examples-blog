1637a18572b567499c4ea6eca94b74812362b368
Alexander <alexander@iMac-Alexander.local>
2024-02-09 13:58:04 +0300

    fulltext search

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import TrigramSimilarity, SearchVector, SearchQuery, SearchRank
 
 from .forms import CommentForm, EmailPostForm, SearchForm
 
@@ -123,9 +123,27 @@ def post_search(request: HttpRequest):
 
     if 'query' in request.GET:
         form = SearchForm(request.GET)
+        # if form.is_valid():
+        #     query = form.cleaned_data['query']
+        #     # search_vector = SearchVector('title', 'body', config="spanish")
+        #     search_vector = SearchVector('title', weight="A") + \
+        #         SearchVector('body', weight="B")
+        #     search_query = SearchQuery(query)
+        #     results = Post.published.annotate(
+        #         search=search_vector, 
+        #         rank = SearchRank(search_vector, search_query)
+        #     ).filter(
+        #         # search=search_query
+        #         rank__gte=0.3
+        #     ).order_by('-rank')
+
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query), 
+            ).filter(
+                similarity__gt=0.1
+            ).order_by('-similarity')
 
     context = {
         'form': form,
